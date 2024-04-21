1d3bcbb1e2c41d9a90496c75522b9ce93f8f5a7a
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-04-21 16:28:04 +0500

    stated learning javascript

---
+++ alerts/app.js
@@ -0,0 +1,23 @@
+alert("Your balance is insufficient")
+
+
+
+// greet your website visitor
+alert("Hey👋 visitors \n wellcome to our website")
+
+
+// error message
+alert("Error!Please enter a valid passward")
+
+
+// using line breaks
+alert("WELcome to js land.... \n Happy Coding!")
+
+// sequence message
+alert("Welcome to js land...")
+
+alert("Happy Coding!")
+
+// message through console
+
+console.log( )
