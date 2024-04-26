79fe39a6d0947da08b9c1b613d0303ce82791c70
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-04-26 09:58:16 +0500

    chapter2

---
+++ chapter2/app.js
@@ -0,0 +1,48 @@
+// initializing a variable
+var userName;
+
+// declaring variable my 
+
+var myName = "Tayyaba Ghulam Muhammad Khatri";
+console.log(myName);
+
+
+// variable in alert box
+var message = "Hello World";
+alert(message);
+
+// student bio
+var studentName = "jhone Doe";
+var studebtAge = "15 years old";
+var course = "Certified Mobile Application Development";
+alert(studentName);
+alert(studebtAge);
+alert(course);
+
+// pizza
+
+var pizza = `
+            PIZZA
+            PIZZ
+           PIZ
+          PI
+         P`;
+   alert(pizza);
+   
+//    email
+var email = "example@example.com";
+alert("My email address is "+ email);
+ 
+// book 
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+
+// document
+document.write("Yeah I can write HTML content through JavaScript");
+ 
+
+// symbols
+
+var designs = "___________*********__________";
+alert(designs);
