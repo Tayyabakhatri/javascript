ffc67c343c76ffdfc6ac32fe4cfcc657a7ab97ae
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-05-02 07:00:34 +0500

    chapter5

---
+++ chapter5/app.js
@@ -0,0 +1,32 @@
+// task_1
+// sum of numbers
+var num = 3;
+var num_2 = 5;
+var sumOfNumAndNum_2 = num+num_2;
+document.write("sum of " + num + " and" + num_2 +" is " + sumOfNumAndNum_2);
+// task_2
+// subtraction
+var subOfNumAndNum_2 = num_2-num;
+document.write("<br>subtraction of " + num +" and " + num_2 +" is " +subOfNumAndNum_2 );
+// multiplication
+var multiplicationOfNumAndNum_2 = num*num_2;
+document.write("<br>multiplication of " + num +" and " + num_2 +" is " +multiplicationOfNumAndNum_2);
+// division
+var divisionOfNumAndNum_2 = num / num_2;
+document.write("<br>division of " + num +" and " + num_2 +" is " +divisionOfNumAndNum_2);
+// modulus
+var modulusOfNumAndNum_2 = num| num_2;
+document.write("<br>modulus of " + num +" and " + num_2 +" is " + modulusOfNumAndNum_2);
+// task_3
+var mathematicalExpression ;
+document.write("<br>Value after variable declaration is " + mathematicalExpression);
+mathematicalExpression = 5;
+document.write("<br>Initial Value: " + mathematicalExpression);
+mathematicalExpression = mathematicalExpression + 1;
+document.write("<br>Value after Increament is: " + mathematicalExpression);
+mathematicalExpression = mathematicalExpression + 7;
+document.write("<br>Value after addition is: " + mathematicalExpression);
+mathematicalExpression = mathematicalExpression-1;
+document.write("<br>Value after decrement is: "+ mathematicalExpression);
+mathematicalExpression = mathematicalExpression/3;
+document.write("<br>The reminder is: " + mathematicalExpression);
