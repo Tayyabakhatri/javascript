ae1af8a20cda6e47c367c5af4dc21282cc3ad58b
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-05-03 08:04:08 +0500

    table added to chapter5

--- chapter5/app.js
+++ chapter5/app.js
@@ -15,18 +15,47 @@ document.write("<br>multiplication of " + num +" and " + num_2 +" is " +multipli
 var divisionOfNumAndNum_2 = num / num_2;
 document.write("<br>division of " + num +" and " + num_2 +" is " +divisionOfNumAndNum_2);
 // modulus
-var modulusOfNumAndNum_2 = num| num_2;
+var modulusOfNumAndNum_2 = num%num_2;
 document.write("<br>modulus of " + num +" and " + num_2 +" is " + modulusOfNumAndNum_2);
 // task_3
 var mathematicalExpression ;
 document.write("<br>Value after variable declaration is " + mathematicalExpression);
 mathematicalExpression = 5;
 document.write("<br>Initial Value: " + mathematicalExpression);
-mathematicalExpression = mathematicalExpression + 1;
+mathematicalExpression = ++mathematicalExpression ;
 document.write("<br>Value after Increament is: " + mathematicalExpression);
 mathematicalExpression = mathematicalExpression + 7;
 document.write("<br>Value after addition is: " + mathematicalExpression);
-mathematicalExpression = mathematicalExpression-1;
+mathematicalExpression = --mathematicalExpression;
 document.write("<br>Value after decrement is: "+ mathematicalExpression);
-mathematicalExpression = mathematicalExpression/3;
+mathematicalExpression = mathematicalExpression%3;
 document.write("<br>The reminder is: " + mathematicalExpression);
+// movie ticket
+var movieTicketPrice = 600;
+var movie5 = (movieTicketPrice * 5);
+document.write("<br>Total cost of buy 5 tickets to a movie is " + movie5 + "PKR");
+// table of 5
+var numFive = 5;
+var multiplySign = "*"
+var equalSign = "="
+var numOne = 1;
+var numTwo = 2;
+var numThree =3 ;
+var numFour = 4;
+var numFive = 5;
+var numSix = 6;
+var numSeven =7 ;
+var numEight = 8;
+var numNine = 9;
+var numTen = 10;
+document.write("<h1>TABLE OF 5 ")
+document.write( "<br>" +numFive + multiplySign + numOne  + equalSign + (numFive*numOne) );
+document.write( "<br>" +numFive + multiplySign + numTwo  + equalSign + (numFive*numTwo) );
+document.write( "<br>" +numFive + multiplySign + numThree  + equalSign + (numFive*numThree) );
+document.write( "<br>" +numFive + multiplySign + numFour  + equalSign + (numFive*numFour) );
+document.write( "<br>" +numFive + multiplySign + numFive  + equalSign + (numFive*numFive) );
+document.write( "<br>" +numFive + multiplySign + numSix  + equalSign + (numFive*numSix) );
+document.write( "<br>" +numFive + multiplySign + numSeven  + equalSign + (numFive*numSeven) );
+document.write( "<br>" +numFive + multiplySign + numEight  + equalSign + (numFive*numEight) );
+document.write( "<br>" +numFive + multiplySign + numNine  + equalSign + (numFive*numNine) );
+document.write( "<br>" +numFive + multiplySign + numTen  + equalSign + (numFive*numTen) );
