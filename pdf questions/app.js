e2b6581d8987cde9b49f4bd4a72bfcdcdc6e9cbf
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-05-29 23:46:15 +0500

    practice

--- pdf questions/app.js
+++ pdf questions/app.js
@@ -1,6 +1,41 @@
-var dishes = prompt("enter any 5 dishes name you like the most")
-var dish1 = console.log(dishes.split(","),("  ")[0])
-var dish2 = console.log(dishes.split(","),("  ")[1])
-var dish3 = console.log(dishes.split(","),("  ")[2])
-var dish4 = console.log(dishes.split(","),("  ")[3])
-var dish5 = console.log(dishes.split(","),("  ")[4])
+// var dishes = prompt("enter any 5 dishes name you like the most")
+// var dish1 = console.log(dishes.split(","),("  ")[0])
+// var dish2 = console.log(dishes.split(","),("  ")[1])
+// var dish3 = console.log(dishes.split(","),("  ")[2])
+// var dish4 = console.log(dishes.split(","),("  ")[3])
+// var dish5 = console.log(dishes.split(","),("  ")[4])
+
+// loops
+// var colors = ["blue","pink","brown","brown","black","purple","black","white","brown","brown"]
+
+// console.log(colors.lastIndexOf("brown"));
+// var userInput =[ prompt("enter your favourite colors:"),
+// prompt("enter your favourite colors:"),
+// prompt("enter your favourite colors:")
+// ]
+// console.log(userInput[1]);
+// console.log(userInput)
+var ques1 = +prompt("2+2=");
+var ans = [ques1,ques2,ques3];
+if(ques1===4){
+    console.log("go ahead");
+}
+else{
+    console.log("danger")
+}
+var ques2 = prompt("what is next to 'r' in alphabets");
+if(ques2=="s"){
+    console.log("keep going");
+}
+else{
+    console.log("noooooooo");
+}
+var ques3 = prompt("do you know java script?");
+if (ques3== "yes"){
+    console.log("done")
+}
+else{
+    console.log("you failed")
+}
+
+
