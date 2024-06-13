74e8a9379df7bf1f7ed8c2ad76933300c72329ce
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-06-13 23:39:26 +0500

    modified

--- pdf questions/app.js
+++ pdf questions/app.js
@@ -169,65 +169,16 @@
 //   }
 // }
 
-// 6. Write a JavaScript program that accepts a number as input and inserts dashes (-) between each even number. For example if you accept 025468 the output should be 0-254-6-8.
-var userNum = +prompt("enter number (seperated by space").split("");
-var result;
-for(var i = 0; i<userNum.length;i++){
-   var num =userNum[i]
-    for(var j=0; j<userNum.length;j++){
-        if(userNum[j]%2==0){
-            result=userNum[j] + "-"
-        }
-        else{
-            result=userNum[j]
-        }
-    }
+// 6. Write a JavaScript program that accepts a number as input and
+//inserts dashes (-) between each even number. For example 
+//if you accept 025468 the output should be 0-254-6-8.
+var userNum = prompt("enter number (seperated by space)");
+var splittedArr = userNum.split(" ");
+
+for (var i = 0; i < splittedArr.length; i++) {
+  if (splittedArr[i] % 2 == 0) {
+  
+    // var result = "-" + splittedArr[i];
+    // console.log(result);
+  }
 }
-console.log(result)
-
-
-
-
-// // Declare and initialize the original array
-// var arr1 = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
-
-// // Declare an empty array to store the sorted result
-// var arr2 = [];
-
-// // Initialize variables to track the minimum, maximum, and position in the original array
-// var min = arr1[0];
-// var pos;
-// var max = arr1[0];
-
-// // Find the maximum value in the original array
-// for (i = 0; i < arr1.length; i++) {
-//     if (max < arr1[i]) max = arr1[i];
-// }
-
-// // Selection sort algorithm: Iterate over the original array
-// for (var i = 0; i < arr1.length; i++) {
-//     // Iterate over the original array to find the minimum element
-//     for (var j = 0; j < arr1.length; j++) {
-//         // Check if the element is not marked as processed (not equal to "x")
-//         if (arr1[j] != "x") {
-//             // Find the minimum element and its position in the original array
-//             if (min > arr1[j]) {
-//                 min = arr1[j];
-//                 pos = j;
-//             }
-//         }
-//     }
-//     // Store the minimum element in the sorted array
-//     arr2[i] = min;
-
-//     // Mark the minimum element as processed by replacing it with "x" in the original array
-//     arr1[pos] = "x";
-
-//     // Reset min to the maximum value for the next iteration
-//     min = max;
-// }
-
-// // Output the sorted array
-// console.log(arr2);
-
-
