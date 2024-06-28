2b6dd24a8f8cef11a0700528f8e91e14e35a9e0e
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-06-28 09:40:22 +0500

    chapter 21-25

---
+++ chapter 21-25/app.js
@@ -0,0 +1,70 @@
+// 1. Write a program that takes two user inputs for first and
+// last name using prompt and merge them in a new variable
+// titled fullName. Greet the user using his full name.
+
+// var userFirstName = prompt("hey 👋, Enter your first name:")
+// var userlastName = prompt("hey 👋, Enter your last name:")
+// var firstNameTittled = userFirstName[0].toUpperCase() +userFirstName.slice(1).toLowerCase();
+// var lastNameTittled = userlastName[0].toUpperCase ()+userlastName.slice(1).toLowerCase();
+// alert(firstNameTittled+" "+lastNameTittled)
+
+
+// 2. Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+
+// var userFavMobile = prompt("Enter you favourite mobile phone :")
+// var strLength = userFavMobile.length;
+// document.write("My favourite mobile phone is "+userFavMobile+" srting Length is "+strLength)
+
+// 3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser .
+
+// document.write("<br><br>")
+// var name = "pakistan";
+// var index = name.indexOf("n")
+// document.write("Index of 'n' in the string "+name+" is "+index)
+
+// 4. Write a program to find the last index of letter “l” in the
+// word “Hello World” and display the result in your browser.
+// var greet = "hello world";
+// var findingIndex = greet.lastIndexOf("l") 
+// document.write("string: " + greet+"<br>")
+// document.write("last index of 'l' :" + findingIndex)
+
+
+// 5. Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+
+//method 1
+// var word = "pakistani";
+// var charOfIndex3 = word[3]
+// document.write("string: " + word +"<br>" +`${"character at index 3 :" + charOfIndex3}`);
+
+//method 2
+// var word = "pakistani";
+// var findingChar = word.charAt(3)
+// document.write("string: " + word +"<br>" +`${"character at index 3 :" + findingChar}`);
+
+//method 3 
+// var word = "pakistani";
+// index = word.slice(3,4)
+// document.write("string: " + word +"<br>" +`${"character at index 3 :" + index}`);
+
+
+// 6. Repeat Q1 using string concat() method.
+//1.  Write a program that takes two user inputs for first and
+// last name using prompt and merge them in a new variable
+// titled fullName. Greet the user using his full name.
+
+
+//  var userFirstName = prompt("hey 👋, Enter your first name:")
+//  var userlastName = prompt("hey 👋, Enter your last name:")
+//  var firstNameTittled = userFirstName[0].toUpperCase() +userFirstName.slice(1).toLowerCase();
+//  var lastNameTittled = userlastName[0].toUpperCase ()+userlastName.slice(1).toLowerCase();
+//  var concatenat = firstNameTittled.concat(lastNameTittled)
+//  document.write(concatenat)
+
+// 7. Write a program to replace the “Hyder” to “Islam” in the
+// word “Hyderabad” and display the result in your browser.
+var city1 = "Hyderabad";
