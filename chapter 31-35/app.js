3867142ec151567c1ae362e0693f3d0483107cdd
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-07-04 00:36:25 +0500

    chapter 31-35

---
+++ chapter 31-35/app.js
@@ -0,0 +1,31 @@
+// 1. Write a program that displays current date and time in
+// your browser.
+
+
+var today = new Date();
+document.write(today)
+document.write("<hr>")
+// 2. Write a program that alerts the current month in words.
+// For example December.
+
+
+// var today = new Date();
+// var month = today.getMonth();
+// var months =["jan","feb","mar","Apr","May","june","july","aug","sep","oct","nov","dec"]
+// alert("Current month :"+ months[month])
+
+
+document.write("<hr>")
+// 3. Write a program that alerts the first 3 letters of the current
+// day, for example if today is Sunday then alert will show
+// Sun.
+
+
+// var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var today = new Date();
+// var day = today.getDay();
+// var str = day.toString()
+// alert(days[str])
+
+// 4. Write a program that displays a message “It’s Fun day” if
+// its Saturday or Sunday today.
