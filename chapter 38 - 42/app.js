f55660be87f480638594e0c2b8671d009d0a34a0
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-07-15 23:00:41 +0500

    chapter 38 - 42

--- chapter 38 - 42/app.js
+++ chapter 38 - 42/app.js
@@ -49,3 +49,11 @@ function area(foundS) {
 }
 var result = area(foundS);
 console.log(result);
+
+
+
+// 4. Write a function that receives marks received by a student in 3
+// subjects and returns the average and percentage of these
+// marks. there should be 3 functions one is the mainFunction
+// and other are for average and percentage. Call those functions
+// from mainFunction and display result in mainFunction.
