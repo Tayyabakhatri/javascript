cd3338692c7b6aa8e096826b00f58e5fc16f0631
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-07-17 18:04:54 +0500

    chapter 38 -42

--- pdf questions/app.js
+++ pdf questions/app.js
@@ -249,11 +249,46 @@ document.write("<br>");
 // }
 // console.log(daysUntilEvent())
 
-var userConfirm = confirm("do you want to delete this item:");
-if (userConfirm) {
-  console.log("item delete");
-} else {
-  console.log("action cancelled");
-}
+// var userConfirm = confirm("do you want to delete this item:");
+// if (userConfirm) {
+//   console.log("item delete");
+// } else {
+//   console.log("action cancelled");
+// }
 
-
+Swal.fire({
+  title: "Submit your Github username",
+  input: "text",
+  inputAttributes: {
+    autocapitalize: "off"
+  },
+  showCancelButton: true,
+  confirmButtonText: "Look up",
+  showLoaderOnConfirm: true,
+  preConfirm: async (login) => {
+    try {
+      const githubUrl = `
+        https://api.github.com/users/${login}
+      `;
+      const response = await fetch(githubUrl);
+      if (!response.ok) {
+        return Swal.showValidationMessage(`
+          ${JSON.stringify(await response.json())}
+        `);
+      }
+      return response.json();
+    } catch (error) {
+      Swal.showValidationMessage(`
+        Request failed: ${error}
+      `);
+    }
+  },
+  allowOutsideClick: () => !Swal.isLoading()
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire({
+      title: `${result.value.login}'s avatar`,
+      imageUrl: result.value.avatar_url
+    });
+  }
+});
