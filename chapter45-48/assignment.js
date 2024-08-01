b8066ecd2f8b4c3549e09c3b8303183baf4b99ce
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-01 10:00:34 +0500

    chapter 43-48

--- chapter45-48/assignment.js
+++ chapter45-48/assignment.js
@@ -1,6 +1,6 @@
-// 
+//
 // done
-// 
+//
 
 // 3. Display 10 student records in table and each row should contain a delete
 // button. If you click on a button to delete a record, entire row should be
@@ -10,3 +10,15 @@
 // button and decrease on click on decrease button. And show updated counter
 // value in browser. -->
 
+function decreaseCounter(a) {
+  var resultElement = document.getElementById(a);
+  var currentValue = Number(resultElement.innerHTML);
+  if (currentValue > 0) {
+    resultElement.innerHTML = currentValue-1;
+  }
+}
+function increaseCounter(b){
+    var resultElement=document.getElementById(b)
+    var currentValue = Number(resultElement.innerHTML);
+    resultElement.innerHTML=currentValue+1
+}
