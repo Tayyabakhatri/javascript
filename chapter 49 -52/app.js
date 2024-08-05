528d15095f42c561fbd763a6b7ad64c7261b650a
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-05 23:46:34 +0500

    chapter49-52

--- chapter 49 -52/app.js
+++ chapter 49 -52/app.js
@@ -1,3 +1,27 @@
 // 1. Create a signup form and display form data in your web
 // page on submission.
 
+// console.log(name)
+function name(){
+    var name = document.getElementById("name").value
+    var email = document.getElementById("email").value
+    var address = document.getElementById("address").value
+    var PhoneNumber = document.getElementById("Phone-number").value
+    var religion = document.getElementById("religion").value
+    var formData = {
+        name,
+        email,
+        address,
+        PhoneNumber,
+        religion
+    };
+    document.getElementById("form-data").innerHTML=
+    <p>Form Data</p>;
+    <p>Name : ${formData.name}</p>;
+    <p>Email : ${formData.email}</p>;
+    <p>Address : ${formData.address}</p>;
+    <p>Phone number  : ${formData.PhoneNumber}</p>;
+    <p>Religion : ${formData.religion}</p>;
+    return false
+} 
+  
