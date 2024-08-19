cee8fbba8df9f10d1b4815b23c39a97698c6e0fd
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-20 00:51:32 +0500

    modified

--- To-Do-list/app.js
+++ To-Do-list/app.js
@@ -1,16 +1,5 @@
-function mainDiv(){
-var a = document.getElementById("mainDiv") 
-a.className="main-Div"
+function div(){
+    var a= document.getElementById("mainDiv")
+    a.className="div"
 }
-mainDiv()
-
-function innerDiv(){
-    var innerDiv = document.getElementById("innerDiv")
-    innerDiv.className="inner-div"
-}
-innerDiv()
-
-function card(){
-    var card = document.getElementById("card")
-    card.className="card"
-}
+div()
