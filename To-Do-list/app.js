720dd57ae7fedca68c0cc3a7b30874ac9d2671c0
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-20 00:43:20 +0500

    modified

---
+++ To-Do-list/app.js
@@ -0,0 +1,16 @@
+function mainDiv(){
+var a = document.getElementById("mainDiv") 
+a.className="main-Div"
+}
+mainDiv()
+
+function innerDiv(){
+    var innerDiv = document.getElementById("innerDiv")
+    innerDiv.className="inner-div"
+}
+innerDiv()
+
+function card(){
+    var card = document.getElementById("card")
+    card.className="card"
+}
