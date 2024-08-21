fcfa8e1e37682419c819b9a909272e7bef273bda
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-21 07:06:57 +0500

    modified

--- To-Do-list/app.js
+++ To-Do-list/app.js
@@ -1,5 +1,22 @@
-function div(){
-    var a= document.getElementById("mainDiv")
-    a.className="div"
+function abc() {
+    var data = document.getElementById("input")
+    var text = data.value
+    var ul = document.getElementById("ul")
+    var li = document.createElement("li")
+    var content = document.createTextNode(text)
+    li.appendChild(content)
+    li.setAttribute("type", "square")
+
+    li.className = "listItems"
+    ul.appendChild(li)
+    data.value = ""
 }
-div()
+
+
+
+
+function stylingInput() {
+    var a = document.getElementById("input")
+    a.className = "inputfield"
+}
+stylingInput()
