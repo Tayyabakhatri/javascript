136ab2b458eb61eacef39c5bc238e9cc79138ca4
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-25 00:02:38 +0500

    assignment

--- chapter58-67/app.js
+++ chapter58-67/app.js
@@ -5,3 +5,17 @@ var b = document.children[0].children[1].children[0].children[2];
 console.log(b)
 var c = document.children[0].children[1].children[0].children[2].children;
 console.log(c)
+var d = document.getElementById("first-name");
+d.value="Tayyaba"
+var e = document.getElementById("last-name")
+e.value="khatri"
+var f= document.getElementById("email")
+f.value="Tayyabakhatri@gmail.com"
+
+
+var g = document.children[0].children[1].children[0].children[1].nodeType
+console.log(g)
+
+var h = document.children[0].children[1].children[0].children[2].childNodes[1].nodeType
+console.log(h)
+var i = document.children[0].children[1].children[0].children[2].childNodes[1].innerHTML="updated text"
