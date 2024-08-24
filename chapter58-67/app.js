b076ff15609673da8374058260c9929e716dc8b8
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-24 23:41:28 +0500

    chapter 58-67

---
+++ chapter58-67/app.js
@@ -0,0 +1,7 @@
+//1
+var a = document.getElementById("main-content");
+//2
+var b = document.children[0].children[1].children[0].children[2];
+console.log(b)
+var c = document.children[0].children[1].children[0].children[2].children;
+console.log(c)
