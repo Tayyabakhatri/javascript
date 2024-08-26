9dc9296b1e3c20a0ff4d7ce1e54dde1653d88310
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-26 23:56:06 +0500

    assignment

--- chapter58-67/app.js
+++ chapter58-67/app.js
@@ -3,23 +3,32 @@ var a = document.getElementById("main-content");
 //2
 var b = document.children[0].children[1].children[0].children[2];
 console.log(b)
-var c = document.children[0].children[1].children[0].children[2].children;
+var c = b.children;
 console.log(c)
 var d = document.getElementById("first-name");
-d.value="Tayyaba"
+d.value = "Tayyaba"
 var e = document.getElementById("last-name")
-e.value="khatri"
-var f= document.getElementById("email")
-f.value="Tayyabakhatri@gmail.com"
+e.value = "khatri"
+var f = document.getElementById("email")
+f.value = "Tayyabakhatri@gmail.com"
 
 
 var g = document.children[0].children[1].children[0].children[1].nodeType
 console.log(g)
-
-var h = document.children[0].children[1].children[0].children[2].childNodes[1].nodeType
+var save = document.children[0].children[1].children[0].children[2].childNodes[1] //storing element in var and then use it
+var h = save.nodeType
 console.log(h)
-var i = document.children[0].children[1].children[0].children[2].childNodes[1].innerHTML="updated text"
-var j= document.children[0].children[1].children[0].children[2].lastChild
-var k= document.children[0].children[1].children[0].children[2].firstChild
+var i = save.innerHTML = "updated text"
+var save2 = document.children[0].children[1].children[0].children[2] //same
+var j = save2.lastChild // elenent child bhi dekh skty hen
+var k = save2.firstChild
 console.log(j)
 console.log(k)
+var l = save2.children[1].nextElementSibling// ager sirf next sibling likhengy to text btainga because of junkartifacts
+console.log(l)
+var m = save2.children[1].previousElementSibling
+console.log(m)
+var n= f.parentElement
+var o =f.parentElement.nodeType
+console.log(n)
+console.log(o)
