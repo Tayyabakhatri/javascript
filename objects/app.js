246d2bfcf756bf4471f29c29795efe35e5ccc7f5
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-09-05 23:21:46 +0500

    modified

--- objects/app.js
+++ objects/app.js
@@ -8,6 +8,11 @@ var employee = {
         return `The Name of employee is ${employee["Employee Name"]} and Age is ${employee["Age"]}. he lives in ${employee["Residence"]} and his salary is ${employee["salary"]}`
     }
 }
+var objectKeys = Object.keys(employee)
+console.log(objectKeys)
+for (var i = 0; i < objectKeys.length; i++) {
+    console.log(employee[objectKeys[i]])//ager keys ki value chahiye to [i] likhna pryga ..otherwise sirf keys la kr dega secondly ager dot notation se get kerty to error ata q k dot notation str nahi leta
+}
 console.log(employee["Get Employee Data"]())
 var arr = [
     {
@@ -39,8 +44,8 @@ var arr = [
 
     }
 ]
-console.table(arr)
+console.table(arr[1])
 //loop on array of object 
 for (var i = 0; i < arr.length; i++) {
-console.log(arr[i])
+    console.log(arr[i])
 }
