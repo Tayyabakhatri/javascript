895f19817d0d4c12e31a8a6ac3ca9a5ace64d67e
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-16 23:26:40 +0500

    modified

--- arrMethodApnaCollege/app.js
+++ arrMethodApnaCollege/app.js
@@ -1,3 +1,4 @@
+import {a} from "./file.js"
 var arr = [1, 3, 2, 4, 5, 6, 7, 8, 90, 49, 69, 89, 99, 67, 40, 13, 54, 64, 21, 54, 75]
 var newArr = arr.forEach((val) => {
     console.log(Math.floor(Math.sqrt(val)))
@@ -24,11 +25,11 @@ var arr = arrValue.forEach(val => {
     console.log(val)
 })
 //using for in loop
-for (key in arrValue) {
-    if (arrValue.hasOwnProperty(key)) {
-        console.log(key + ": " + arrValue[key]);
-    }
-}
+// for (key in arrValue) {
+//     if (arrValue.hasOwnProperty(key)) {
+//         console.log(key + ": " + arrValue[key]);
+//     }
+// }
 function Employee(name, father) {
     this.Name = name,
         this.fatherName = father
@@ -77,10 +78,25 @@ function bye(callBack) {
 } 
 bye(hello)
 //spred operator 
-let nums = [ 1,2,3,4,5,6,7,8,9,0]
+let nums = [ 1,2,3,4,5,6,7,8,9,0];
 let num2 = nums.slice(0)
 let num3 = [].concat(nums)
 let num4 =[...nums,5,5,6,9]
 console.log(num4)
+//iterators in array
+let iter = nums[Symbol.iterator]()
+console.log(iter.next())
+iter.next()
+iter.next()
+console.log(iter.next().value)
+console.log(iter.next().done
+)
+console.log("hetttyye")
+console.log(iter.next())
+//iterators in object
+
+
+
+console.log(a)
 
 
