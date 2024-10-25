55b0a8bbee27592213a2c29701105078b5f5e635
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-25 23:12:53 +0500

    promises

--- promises/app.js
+++ promises/app.js
@@ -24,3 +24,30 @@ async function myFunc(para1) {
     } , 5000)
 }
 myFunc(awaitFunc)
+//passing Array in resolved promise
+let count= [6,7,8,9,10];
+let myPromise=new Promise( (resolved,rejected)=>{
+if(true){
+  resolved(count)
+}
+})
+.then((mesg)=>{
+  let a =  Array.isArray(mesg)
+    console.log(a);
+    mesg.forEach(element => {
+        let b = element
+        console.log(b)
+    });
+
+})
+//passing objectsin promise 
+new Promise((resolved, rejected)=>{
+    let obj ={name:"Tayyaba",Age:25};
+    return rejected(obj)
+}).then((name)=>{
+    console.log({name})
+}).catch(({name,Age,})=>{     //destructuring
+    console.log(name,Age)
+})
+    
+
